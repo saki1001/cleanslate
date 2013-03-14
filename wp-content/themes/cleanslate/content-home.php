ee0f77a2a2eb7079fd91ef614d67a21fc8b0fac0
Saki Sato <sakiisato@gmail.com>
2013-03-14 13:06:26 +0900

    adding all wordpress files and my edited version of toolbox called cleanslate

---
+++ wp-content/themes/cleanslate/content-home.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The template for the homepage.
+ *
+ * @package CleanSlate
+ * @since CleanSlate 0.1
+ */
+?>
+
+<?php
+    // Slideshow set in Home page
+    the_content();
+?>
---
+++ wp-content/themes/cleanslate/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package CleanSlate
+ * @since CleanSlate 0.1
+ */
+
+get_header(); ?>
+        
+        <section id="content" role="main">
+            <?php
+                get_template_part( 'content', 'home' );
+            ?>
+        </section>
+<?php get_footer(); ?>
